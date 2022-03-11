b0c60f31751fb403926a0ffb5bc8d7af4b0e0db8
RISHABH <gguptarishabh82@gmail.com>
2022-03-12 02:04:29 +0530

    Add New Contact

---
+++ UC2_AddNewContact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem_Day9
+{
+    public class UC2_AddNewContact
+    {
+        public static void AddContact()
+        {
+            string Name;
+            Console.WriteLine("Enter FIRST name and LAST name");
+            Name = Console.ReadLine();
+            string Address;
+            Console.WriteLine("Enter Address city state zip");
+            Address = Console.ReadLine();
+            string EmailId;
+            Console.WriteLine("Enter EmailId");
+            EmailId = Console.ReadLine();
+            long Phonenumber;
+            Console.WriteLine("Enter your PhoneNumber");
+            Phonenumber = Convert.ToInt64(Console.ReadLine());
+
+        }
+    }
+}
